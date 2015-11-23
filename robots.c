79fb735a937d4ecf067ff253a6df48f677d4240c
Jorge Edson Ribeiro da Silva Neto <jorge.ed.ribeiro00@gmail.com>
2015-11-22 23:30:36 -0300

    Iniciado o programa

---
+++ robots.c
@@ -0,0 +1,11 @@
+/*Progama: Robots.c(ex27);
+  Funcao: Rodar o jogo Robots;
+  Autores: Jorge Edson Ribeiro da Silva Neto & Joao Gabriel Gouveia de Souza Brito;
+  Contatos: jorge.ed.ribeiro00@gmail.com & gabriel.gouveia@live.com;*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
